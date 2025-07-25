d7dc2fe66a08e1122165398a0b4f5979e88f70eb
ShubhamGarg_6178 <shubhamgarg6178@gmail.com>
2025-07-25 22:52:40 +0530

    leanred about strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "shubham"
+const age = 20
+//console.log(`my name is ${name} and my age is ${age}`)
+
+const about = new String('coding')
+console.log(about.charAt(2))
+console.log(about.substring(0,2))
